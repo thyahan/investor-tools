bbb326e3ea19da3b9d29e2511d4374ff2aec999b
Yutthana Yahan <yutthana_yah@truecorp.co.th>
2021-01-06 23:18:37 +0700

    refactor: layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import {FC} from 'react';
-import Layout from 'components/Layout';
 
 type IProps = {};
 
 const Index: FC<IProps> = props => {
   return (
-    <Layout>
-      <div className="w-full h-full p-4 border border-gray-200 rounded-md shadow-md">
-        <p className="text-xl text-gray-500">HELLO</p>
-      </div>
-    </Layout>
+    <div className="w-full h-full p-4 border border-gray-200 rounded-md shadow-md">
+      <p className="text-xl text-gray-500">HELLO</p>
+    </div>
   );
 };
 
